e5a7d9a95e2507a8efdbbac103bdb7736b1fde70
Başak <96747702+basaksts@users.noreply.github.com>
2026-05-01 20:01:49 +0300

    update html

--- webapp/camera.py
+++ webapp/camera.py
@@ -2,11 +2,11 @@ import cv2
 
 class VideoCamera:
     def __init__(self):
-<<<<<<< Updated upstream
-        self.video = cv2.VideoCapture("rtsp://192.168.x.x:554/...")
-=======
+
+       # self.video = cv2.VideoCapture("rtsp://192.168.x.x:554/...")
+
         self.video = cv2.VideoCapture(0)
->>>>>>> Stashed changes
+
 
     def get_frame(self):
         success, image = self.video.read()
